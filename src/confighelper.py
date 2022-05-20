37ba5fcb96758f543e67e5d2a0d27bdab2bc0490
mstreutker <mark.streutker@gmail.com>
2022-05-20 23:06:37 +0200

    Updated configparser to look for local config file

--- src/confighelper.py
+++ src/confighelper.py
@@ -3,5 +3,5 @@ import configparser
 # Method to read config file settings
 def read_config():
     config = configparser.ConfigParser()
-    config.read('config.ini')
+    config.read('local.config.ini')
     return config
