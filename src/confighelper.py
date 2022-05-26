a42e98c4fc64c2b7d7ae97372d88cd048f96ecf8
mstreutker <mark.streutker@gmail.com>
2022-05-26 15:03:30 +0200

    Updated relative path config file

--- src/confighelper.py
+++ src/confighelper.py
@@ -3,5 +3,5 @@ import configparser
 # Method to read config file settings
 def read_config():
     config = configparser.ConfigParser()
-    config.read('local.config.ini')
+    config.read('.\local.config.ini')
     return config
