9070a4fa1e555e11088c06162f8797214c3fd1ab
mstreutker <mark.streutker@gmail.com>
2023-05-02 19:51:59 +0200

    added support for multi attachments

--- src/emailhelper.py
+++ src/emailhelper.py
@@ -3,6 +3,9 @@ import confighelper
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
+import io
 
 class email_client:
     def __init__(self):
@@ -14,7 +17,7 @@ class email_client:
         self.password = config['EmailSettings']['password']
         self.recipient = config['EmailSettings']['recipient']
 
-    def sendmail(self, recipient, subject, content, image):
+    def sendmail(self, recipient, subject, content, images):
 
         # add headers
         headers = ["From: " + self.username, "Subject: " + subject, "To: " + recipient,
@@ -22,20 +25,39 @@ class email_client:
         headers = "\r\n".join(headers)
 
         # add general email info
-        msg = MIMEMultipart('alternative')
+        msg = MIMEMultipart('related')
         msg['From'] = self.username
         msg['To'] = recipient
         msg['Subject'] = subject
 
-        # attach image to email
-        img = MIMEImage(image, 'jpg')
-        img.add_header('Content-ID', 'image')
-        msg.attach(img)
+        # Define the HTML content of the email
+        html = """
+        <html>
+        <body>
+        """
+        html = html + f"<p>{content}</p>"
 
-        # embed image in email
-        text = f"{content}<img src='cid:image'>"
-        bodytext = MIMEText(text, 'html')
+        for index, image in enumerate(images):
+            # embed image in email
+            text = f"<p><img src='cid:image{index}'></p>"
+            # bodytext = MIMEText(text, 'html')
+            # msg.attach(bodytext)
+            html = html + text
+
+        html = html + """
+        </body>
+        </html>
+        """
+        bodytext = MIMEText(html, 'html')
         msg.attach(bodytext)
+
+        for index, image in enumerate(images):
+            # attach image to email
+            img = MIMEImage(image, 'jpg')
+            img.add_header('Content-ID', f'image{index}')
+            img.add_header('Content-Disposition','inline', filename=f'image{index}')
+            
+            msg.attach(img)
 
         # connect to server
         session = smtplib.SMTP(self.smtp_server, self.smtp_port)
@@ -51,3 +73,33 @@ class email_client:
 
         # quit session
         session.quit
+
+class email_helper:      
+
+    def df_to_plot_table (self, df):
+        fig, ax = plt.subplots()
+  
+        # hide axes
+        fig.patch.set_visible(False)
+        ax.axis('off')
+        ax.axis('tight')
+        table = ax.table(cellText=df.values, colLabels=df.columns,  loc='center')
+        fig.tight_layout()
+
+        buf = io.BytesIO()
+        plt.savefig(buf, format='jpg')
+        buf.seek(0)
+        data = buf.read()
+
+        return data
+        
+    def df_to_plot_bar(self, df):
+        rcParams.update({'figure.autolayout': True})
+        df.sort_values(by='margin', ascending=True).plot(kind='bar', y=['margin'], x='ticker')
+        
+        buf = io.BytesIO()
+        plt.savefig(buf, format='jpg')
+        buf.seek(0)
+        data = buf.read()
+
+        return data        
